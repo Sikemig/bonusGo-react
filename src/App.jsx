dfce00be63d3a6bfa5f3a6baa271d7c1e1af7cc9
Sikemig <sikemig@gmail.com>
2025-04-25 17:32:36 +0200

    conexion api

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Dashboard from './components/Dashboard';
-import ProtectedRoute from './utils/ProtectedRoute';
+import ProtectedRoute from './utils/ProyectedRoute';
 
 export default function App() {
   return (
